db9c59b6c5c885a6b59911874fd11f4d06999774
Feliconut <33282070+Feliconut@users.noreply.github.com>
2019-01-21 15:09:52 +0800

    3.1.1.2 Develop 190121

--- Managebaker Alpha 3.1.1.1/event.js
+++ Managebaker Alpha 3.1.1.1/event.js
@@ -1,27 +1,21 @@
+//fuction getevent() {};
+currentDate = {
+  id: 20408371,
+  title: "Quiz 1",
+  start: "2018-09-03T13:50:00.000+08:00",
+  end: null,
+  allDay: false,
+  description:
+    '\u003cp\u003eAll questions of Welcome quiz are from summer holiday homework. But from the results, it seems that\u0026nbsp;most of students are below 80% which reflects students\' attitude and foundation of Math that has been taught last year.\u0026nbsp; \u003cspan id="selectionBoundary_1536554627952_9767587885103869" class="rangySelectionBoundary"\u003e\u003c/span\u003e\u003cspan id="selectionBoundary_1536554626024_7705249055866562" class="rangySelectionBoundary"\u003e\u003c/span\u003e\u003c/p\u003e',
+  hint_url: "/student/classes/11014438/events/20408371/hint",
+  editable: false,
+  ia_copy: false,
+  backgroundColor: "#33C",
+  textColor: "#fff",
+  category: "Summative Assessment",
+  url: "/student/classes/11014438/assignments/20408371"
+};
 
+//classid
 
-fuction getevent(){
-currentDate=
-}
-
-{
-    "id": 20408371,
-    "title": "Quiz 1",
-    "start": "2018-09-03T13:50:00.000+08:00",
-    "end": null,
-    "allDay": false,
-    "description": "\u003cp\u003eAll questions of Welcome quiz are from summer holiday homework. But from the results, it seems that\u0026nbsp;most of students are below 80% which reflects students' attitude and foundation of Math that has been taught last year.\u0026nbsp; \u003cspan id=\"selectionBoundary_1536554627952_9767587885103869\" class=\"rangySelectionBoundary\"\u003e\u003c/span\u003e\u003cspan id=\"selectionBoundary_1536554626024_7705249055866562\" class=\"rangySelectionBoundary\"\u003e\u003c/span\u003e\u003c/p\u003e",
-    "hint_url": "/student/classes/11014438/events/20408371/hint",
-    "editable": false,
-    "ia_copy": false,
-    "backgroundColor": "#33C",
-    "textColor": "#fff",
-    "category": "Summative Assessment",
-    "url": "/student/classes/11014438/assignments/20408371"
-}
-
-
-
-classid
-
-eventid
+//eventid
