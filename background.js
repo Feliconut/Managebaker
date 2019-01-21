a33ad34cf9eb870e74c343df4f3867725478fe50
martinwang2002 <martinwang2002@gmail.com>
2019-01-22 07:22:17 +0800

    3.1.1.2-2 Develop 190121
    event data process

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-chrome.runtime.onInstalled.addListener(function() {
+//第一次安装
+chrome.runtime.onInstalled.addListener(function () {
   //第一次安装
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//Active Tab
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == "complete") {
     var url = tab.url;
     var patt1 = new RegExp("student");
@@ -11,11 +13,15 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
       //in MB
       if (url.lastIndexOf("student") + 9 > url.length) {
         //dashboard
-        alert("dashboard");
+        chrome.tabs.sendMessage(tabId, {
+          type: "dashboard"
+        });
       }
       if (patt2.test(url)) {
-        alert("assignment");
         //assignment
+        chrome.tabs.sendMessage(tabId, {
+          type: "assignment"
+        });
       }
     }
   }
