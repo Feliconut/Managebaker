9c5440b22bb373177543fe53c880654c945e5e93
Feliconut <33282070+Feliconut@users.noreply.github.com>
2019-01-22 12:42:23 +0800

    Develop 190122 C (dataIO still working)

--- event.js
+++ event.js
@@ -25,13 +25,287 @@ classid
 eventid
 */
 
+
+
 $(document).ready(function () {
 
-    var data = getData((data)=>{
-        data.forEach((event)=> {
-        
-        localforage.setItem(event.id, event);
 
+
+    var data = [{
+
+        "id": 20517049,
+
+        "title": "IA - Christmas holiday homework",
+
+        "start": "2019-01-02T21:00:00.000+08:00",
+
+        "end": null,
+
+        "allDay": false,
+
+        "description": "\u003cp\u003e\u003cspan data-pwa-id=\"pwa-0CD8DEB11D90ECBE3CB466B7200A6A53\" data-pwa-category=\"\" data-pwa-hint=\"Unknown word: 请完成之前布置的IA，这里老师重新把题目放上去，同时，大家可以根据老师附件的文件来自查一下写的时候是否已经wang老师上课\" data-pwa-suggestions=\"Wang~vinegar~Wong~wink~Wenger\" data-pwa-dictionary-word=\"请完成之前布置的IA，这里老师重新把题目放上去，同时，大家可以根据老师附件的文件来自查一下写的时候是否已经wang老师上课\"\u003e请完成之前布置的IA，这里老师重新把题目放上去，同时，大家可以根据老师附件的文件来自查一下写的时候是否已经按照\u003c/span\u003e\u003cspan data-pwa-id=\"pwa-0CD8DEB11D90ECBE3CB466B7200A6A53\" data-pwa-category=\"\" data-pwa-hint=\"Unknown word: 请完成之前布置的IA，这里老师重新把题目放上去，同时，大家可以根据老师附件的文件来自查一下写的时候是否已经wang老师上课\" data-pwa-suggestions=\"Wang~vinegar~Wong~wink~Wenger\" data-pwa-dictionary-word=\"请完成之前布置的IA，这里老师重新把题目放上去，同时，大家可以根据老师附件的文件来自查一下写的时候是否已经wang老师上课\"\u003e老师上课讲的评分标准去写。\u003c/span\u003e\u003c/p\u003e\u003cp\u003e\u003cspan data-pwa-id=\"pwa-0CD8DEB11D90ECBE3CB466B7200A6A53\" data-pwa-category=\"\" data-pwa-hint=\"Unknown word: 请完成之前布置的IA，这里老师重新把题目放上去，同时，大家可以根据老师附件的文件来自查一下写的时候是否已经wang老师上课\" data-pwa-suggestions=\"Wang~vinegar~Wong~wink~Wenger\" data-pwa-dictionary-word=\"请完成之前布置的IA，这里老师重新把题目放上去，同时，大家可以根据老师附件的文件来自查一下写的时候是否已经wang老师上课\"\u003e作业递交方式：turnitin， 上传word. Class iD 19921566\u0026nbsp; key: qinchenhl2\u003cbr\u003e\u003c/span\u003e\u003c/p\u003e",
+
+        "hint_url": "/student/classes/11014438/events/20517049/hint",
+
+        "editable": false,
+
+        "ia_copy": false,
+
+        "backgroundColor": "#205C00",
+
+        "textColor": "#fff",
+
+        "category": "Internal Assessment (IA)",
+
+        "url": "/student/classes/11014438/assignments/20517049"
+
+    }, {
+
+        "id": 20517230,
+
+        "title": "圣诞迎新假作业：语言的演变专题研究",
+
+        "start": "2019-01-02T23:00:00.000+08:00",
+
+        "end": null,
+
+        "allDay": false,
+
+        "description": "\u003cp\u003e\u003cspan data-pwa-id=\"pwa-732987D9E625EE12B55FEE4ED7788BAA\" data-pwa-category=\"\" data-pwa-hint=\"Unknown word: 请各组在MB上传ppt及其他相关材料\" data-pwa-suggestions=\"\" data-pwa-dictionary-word=\"请各组在MB上传ppt及其他相关材料\" class=\"pwa-mark-done\"\u003e请各组在DDL之前（Jan 2, 11PM）在MB上传ppt\u003c/span\u003e\u003c/p\u003e\r\n\u003cp\u003ePPT至少包括：标题页（包括组员名字和分工）、每页页眉/页脚、目录页、小结页、参考文献页\u003c/p\u003e\r\n\u003cp\u003e请根据FOA评估标准\u003cstrong\u003e（本次侧重于理论深度，形式上可以不创新）\u003c/strong\u003e\u0026本单元核心主题“文化背景/语境中的语言”，选取有意思的语言现象进行探究，大胆尝试与实践。\u003c/p\u003e\u003cp\u003e每组展示时间：20分钟\u003c/p\u003e\r\n\u003cp\u003e分组表如下，曾灏请和顾泉、宋可仰、欧阳慕风一组。\u003c/p\u003e\r\n\u003cfigure\u003e\u003cimg src=\"/attachments/BAh7CEkiCGdpZAY6BkVUSSIuZ2lkOi8vbWFuYWdlYmFjL0Fzc2V0LzQyMTQ2NTE0P2V4cGlyZXNfaW4GOwBUSSIMcHVycG9zZQY7AFRJIgxkZWZhdWx0BjsAVEkiD2V4cGlyZXNfYXQGOwBUMA==--57b53cac0e058ae2815a69854c8414878aa22243\" data-image=\"BAh7CEkiCGdpZAY6BkVUSSIuZ2lkOi8vbWFuYWdlYmFjL0Fzc2V0LzQyMTQ2NTE0P2V4cGlyZXNfaW4GOwBUSSIMcHVycG9zZQY7AFRJIgxkZWZhdWx0BjsAVEkiD2V4cGlyZXNfYXQGOwBUMA==--57b53cac0e058ae2815a69854c8414878aa22243\"\u003e\u003c/figure\u003e\r\n\u003cfigure\u003e\u003cimg src=\"/attachments/BAh7CEkiCGdpZAY6BkVUSSIuZ2lkOi8vbWFuYWdlYmFjL0Fzc2V0LzQyMTQ2NTE1P2V4cGlyZXNfaW4GOwBUSSIMcHVycG9zZQY7AFRJIgxkZWZhdWx0BjsAVEkiD2V4cGlyZXNfYXQGOwBUMA==--f7d5df2b59898b6045fa6b0938a932077dabad96\" data-image=\"BAh7CEkiCGdpZAY6BkVUSSIuZ2lkOi8vbWFuYWdlYmFjL0Fzc2V0LzQyMTQ2NTE1P2V4cGlyZXNfaW4GOwBUSSIMcHVycG9zZQY7AFRJIgxkZWZhdWx0BjsAVEkiD2V4cGlyZXNfYXQGOwBUMA==--f7d5df2b59898b6045fa6b0938a932077dabad96\"\u003e\u003c/figure\u003e\r\n\u003cp\u003e主题探究及学习成果登记表链接如下\u003c/p\u003e\r\n\u003cp\u003e\u003ca href=\"https://docs.qq.com/sheet/DWG9RZlVPUVF3b1dZ?tdsourcetag=s_macqq_aiomsg\u0026tab=BB08J2\"\u003ehttps://docs.qq.com/sheet/DWG9...\u003c/a\u003e\u003c/p\u003e",
+
+        "hint_url": "/student/classes/11014390/events/20517230/hint",
+
+        "editable": false,
+
+        "ia_copy": false,
+
+        "backgroundColor": "#fff100",
+
+        "textColor": "#000",
+
+        "category": "Summative Assessment",
+
+        "url": "/student/classes/11014390/assignments/20517230"
+
+    }]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+var db; // 全局的indexedDB数据库实例。
+
+var dbVersion;
+
+//1\. 获取IDBFactory接口实例（文档地址： https://developer.mozilla.org/en-US/docs/Web/API/IDBFactory）
+
+var indexedDB =
+
+    window.indexedDB ||
+
+    window.webkitIndexedDB ||
+
+    window.mozIndexedDB ||
+
+    window.msIndexedDB;
+
+
+
+if (!indexedDB) {
+
+    console.log('你的浏览器不支持IndexedDB');
+
+}
+
+
+
+var IDBOpenDBRequest = indexedDB.open('Managebaker');
+
+
+
+// 3\. 对打开数据库的事件进行处理
+
+// 打开数据库成功后，自动调用onsuccess事件回调。
+
+IDBOpenDBRequest.onsuccess = function (e) { };
+
+// 打开数据库失败
+
+IDBOpenDBRequest.onerror = function (e) {
+
+    console.log(e.currentTarget.error.message);
+
+};
+
+
+
+// 第一次打开成功后或者版本有变化自动执行以下事件：一般用于初始化数据库。
+
+IDBOpenDBRequest.onupgradeneeded = function (e) {
+
+    db = e.target.result; // 获取到 demoDB对应的 IDBDatabase实例,也就是我们的数据库。
+
+    //Event 表
+
+    if (!db.objectStoreNames.contains("Event")) {
+
+        //如果表格不存在，创建一个新的表格（keyPath，主键 ； autoIncrement,是否自增），会返回一个对象（objectStore）
+
+        var objectStore = db.createObjectStore("Event", {
+
+            keyPath: 'eventid',
+
+            autoIncrement: true
+
+        });
+
+        //指定可以被索引的字段，unique字段是否唯一。类型： IDBIndex
+
+        objectStore.createIndex('title', 'title', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('start', 'start', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('class_id', 'class_id', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('cat', 'cat', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('completed', 'completed', {
+
+            unique: false
+
+        });
+
+
+
+    }
+
+    //classes 表
+
+    if (!db.objectStoreNames.contains("Classes")) {
+
+        //如果表格不存在，创建一个新的表格（keyPath，主键 ； autoIncrement,是否自增），会返回一个对象（objectStore）
+
+        // objectStore就相当于数据库中的一张表。IDBObjectStore类型。
+
+        var objectStore = db.createObjectStore("Classes", {
+
+            keyPath: 'classesid',
+
+            autoIncrement: true
+
+        });
+
+        //指定可以被索引的字段，unique字段是否唯一。类型： IDBIndex
+
+        objectStore.createIndex('cat_1', 'cat_1', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('cat_2', 'cat_2', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('cat_3', 'cat_3', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('cat_4', 'cat_4', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('cat_5', 'cat_5', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('per_1', 'per_1', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('per_2', 'per_2', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('per_3', 'per_3', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('per_4', 'per_4', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('per_5', 'per_5', {
+
+            unique: false
+
+        });
+
+        objectStore.createIndex('per_t', 'per_t', {
+
+            unique: false
 
         });
 
@@ -40,6 +314,47 @@ $(document).ready(function () {
 
 
 
-    });
 
-})
+
+
+    }
+
+};
+
+
+
+// 创建一个事务，类型：IDBTransaction，文档地址： https://developer.mozilla.org/en-US/docs/Web/API/IDBTransaction
+
+var transaction = db.transaction(Event, 'readwrite');
+
+
+
+// 通过事务来获取IDBObjectStore
+
+var store = transaction.objectStore(Event);
+
+
+
+// 往store表中添加数据
+
+var addPersonRequest = store.add(data);
+
+
+
+// 监听添加成功事件
+
+addPersonRequest.onsuccess = function (e) {
+
+    console.log(e.target.result); // 打印添加成功数据的 主键（id）
+
+};
+
+
+
+// 监听失败事件
+
+addPersonRequest.onerror = function (e) {
+
+    console.log(e.target.error);
+
+};
