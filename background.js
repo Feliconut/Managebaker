2a99042f6540f138cb3349b2916102bbe21fb824
Feliconut <33282070+Feliconut@users.noreply.github.com>
2019-01-28 02:36:26 +0800

    190128 Dev A 顶部菜单栏移入左侧utilities选项, 更改部分左侧menu

--- background.js
+++ background.js
@@ -1,15 +1,13 @@
 //第一次安装
-chrome.runtime.onInstalled.addListener(
-  function () {
-    var action_url = chrome.runtime.getURL("1st_run") + '/index.html';
-    chrome.tabs.create({
-      url: action_url
-    });
-
+chrome.runtime.onInstalled.addListener(function() {
+  var action_url = chrome.runtime.getURL("1st_run") + "/index.html";
+  chrome.tabs.create({
+    url: action_url
   });
+});
 
 //Active Tab
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   //判断tab的标题是否有竖线， MB domcontent 从 url 到title
   if (tab.status == "complete" && tab.title.indexOf("|") != -1) {
     var url = tab.url;
@@ -23,12 +21,15 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         chrome.tabs.sendMessage(tabId, {
           type: "dashboard"
         });
-      }
-      if (patt2.test(url)) {
+      } else if (patt2.test(url)) {
         //assignment
         //send assignment to content.js
         chrome.tabs.sendMessage(tabId, {
           type: "assignment"
+        });
+      } else {
+        chrome.tabs.sendMessage(tabId, {
+          type: "other"
         });
       }
     }
--- managebaker.js
+++ managebaker.js
@@ -25,31 +25,59 @@ Event error:
 //从background.js 接受Message并call function
 chrome.runtime.onMessage.addListener(function (request, sender) {
   if ($("body").hasClass("processed")) {
-    return 0
+    return 0;
   } else {
     $("body").addClass("processed");
-    if (request.type == "assignment") {
-      assignment();
-      eventHandler();
-    }
-    if (request.type == "dashboard") {
-      dashboard();
-      //getData((data) => { console.log(data) });
+    switch (request.type) {
+      case "assignment":
+        {
+          assignment();
+          eventHandler();
+          break;
+        }
+      case "dashboard":
+        {
+          dashboard();
+          break;
+        }
+      case "other":
+        {
+          add_general_style();
+          break;
+        }
     }
   }
 });
 
-
-
-
-
 function add_general_style() {
   $(".school-name").attr("href", "javascript:show_todolist();");
-  $(".school-name").html('<font size="5" color="#FF7F00"><b>T O D O</b></font>');
+  $(".school-name").html(
+    '<font size="5" color="#FF7F00"><b>T O D O</b></font>'
+  );
 
+  addUtilTab();
+
+  function addUtilTab() {
+    var pathString =
+      "M 401 64 Q 170 84 150 300 Q 164 538 400 550 Q 571 535 604 382 L 664 401 Q 659 320 601 252 Q 517 277 462 337 L 532 361 Q 511 464 401 465 Q 248 447 241 299 Q 247 167 400 150 L 401 133 Q 227 150 221 299 Q 235 464 395 484 Q 520 479 549 369 L 587 379 Q 542 523 404 531 Q 187 527 173 300 Q 185 106 399 82 Z";
+    var groupsTab = $("#menu > ul > li:nth-child(11)");
+    var utilitiesTab = $(
+      '<li id="utilTab1" class="parent utilities"><a><svg baseProfile="full" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="100 0 560 600" preserveAspectRatio="xMinYMin meet"><path fill-rule="evenodd" fill="#859BBB" d="' +
+      pathString +
+      '"></path></svg>Utilities</a><ul id="utilTab2" class="nav" data-max-height="129px" style="max-height: 0px;"><li><a href="/student/profile">My Profile </a></li><li><a href="https://accounts.managebac.cn/">Launchpad </a></li><li><a href="/help">Help &amp; Support </a></li><li id="utilTab3"><a id="utilTab3" href="/logout">Logout </a></li></ul></li>'
+    );
+    utilitiesTab.insertAfter(groupsTab);
+    utilitiesTab.click(() => {
+      var $1 = $("#utilTab1");
+      var $2 = $("#utilTab2");
+      $1.toggleClass("opened");
+      $2.attr(
+        "style",
+        $1.hasClass("opened") ? "max-height: 129px" : "max-height: 0px"
+      );
+    });
+  }
 }
-
-
 
 function add_dashboard_Style() {
   add_general_style();
