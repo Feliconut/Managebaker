e43d926052ab0e9675ac4b005b8383c8a04c550e
Feliconut <33282070+Feliconut@users.noreply.github.com>
2019-03-01 22:43:43 +0800

    Event Handler Fix

--- core/background.js
+++ core/background.js
@@ -63,6 +63,8 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     activeTab = tabId;
     //判断tab的标题是否有竖线， MB domcontent 从 url 到title
     if (tab.status == "complete" && tab.title.indexOf("|") != -1) {
+      //所有的代码从这里开始，到这里页面才完成加载。
+      eventHandler()
       console.log(tab_Id)
       var patt1 = new RegExp("student/?$"); //dashboard
       var patt2 = new RegExp("student/classes/[0-9]+/assignments"); //assignments
@@ -98,6 +100,8 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 //Receives command and control the LocalStorage
 chrome.runtime.onMessage.addListener(function storageManager(request, sender, callback) {
   import(RUNTIME_PATH + "lib/localforage.min.js").then(() => {
+
+    console.log(request)
     switch (request.method) {
       case "get":
         {
@@ -105,9 +109,9 @@ chrome.runtime.onMessage.addListener(function storageManager(request, sender, ca
           var event = request.event_id;
           var length = event.length;
           for (var i = 0; i < length; i++) {
-            (function (i) {
-              var id = request.event_id[i];
-              localforage.getItem(id).then(function (value) {
+            var id = request.event_id[i];
+
+            localforage.getItem(id).then(function (value) {
                 if (value.complete == 1) {
                   event_completed.push(id);
                 }
@@ -119,83 +123,100 @@ chrome.runtime.onMessage.addListener(function storageManager(request, sender, ca
                   });
                 }
               })
-            })(i);
+              .catch(function (err) {
+                console.log(err)
+              })
+
           }
-          break;
         }
+
       case "change_complete_status":
         {
-          var event_id = request.event_id;
-          localforage.getItem(event_id).then(function (value) {
-            var jsonObj;
-            if (value.complete == 1) {
-              jsonObj = value;
-              jsonObj.complete = 0;
-              localforage.setItem(event_id, jsonObj);
-            } else if (value.complete == 0) {
-              jsonObj = value;
-              jsonObj.complete = 1;
-              localforage.setItem(event_id, jsonObj);
-            }
-          }).catch(function (err) {
-            console.log(err)
-          })
-          break;
+
+          var length = request.event_id.length;
+          for (var i = 0; i < length; i++) {
+            event = request.event_id[i]
+
+            localforage.getItem(event).then(function (value) {
+              var jsonObj;
+              if (value.complete == 1) {
+                jsonObj = value;
+                jsonObj.complete = 0;
+                localforage.setItem(event, jsonObj);
+              } else if (value.complete == 0) {
+                jsonObj = value;
+                jsonObj.complete = 1;
+                localforage.setItem(event, jsonObj);
+              }
+            }).catch(function (err) {
+              console.log(err)
+            })
+          }
         }
+
     }
   })
-});
+})
 
-function eventHandler() {
-  /*
-    var startDate = new Date();
-    var endDate = new Date();
-    //long query
-    if (modeBool == "1") {
-      startDate.Add(-1, "y");
-      endDate.Add(1, "y");
-      //short query
-    } else {
-      startDate.Add(-1, "M");
-      endDate.Add(1, "M");
-    }
-    endDate.Add(-1, "d");
-    */
+function eventHandler(modeBool = 0) {
+  Promise.all([
+      import(RUNTIME_PATH + "lib/localforage.min.js"),
+      import(RUNTIME_PATH + "lib/jquery-3.3.1.js")
+    ]).then(() => {
+        console.log('enterEventHandler')
+        import('../lib/usefulUtil.js').then((a) => {
+          a.dateEnhance.init()
+          a.sayHello('fuck')
 
-  var url = "https://qibaodwight.managebac.cn/student/events.json";
-  var dateData = {
-    /*
-    start: startDate.Format("yyyy-MM-dd"),
-    end: endDate.Format("yyyy-MM-dd")
-    */
-    start: "2018-01-01",
-    end: "2020-01-01"
-  };
 
-  $.get(
-    url,
-    dateData,
-    function (result, status) {
-      result.forEach(event => {
-        if (typeof event.id != "number") {
-          return;
-        }
-        var event_data = {
-          title: event.title,
-          start: event.start,
-          url: event.url,
-          complete: "1",
-          category: "",
-          score: {
-            get: 0,
-            total: 0
+
+          var startDate = new Date();
+          var endDate = new Date();
+          //long query
+          if (modeBool == 0) {
+            startDate.Add(-1, "y");
+            endDate.Add(1, "y");
+            //short query
+          } else {
+            startDate.Add(-1, "M");
+            endDate.Add(1, "M");
           }
-        };
-        localforage.setItem(String(event.id), event_data).then(function (value) {});
-      });
-      return 1;
-    },
-    "json"
-  );
+          endDate.Add(-1, "d");
 
-}
+
+          var url = "https://qibaodwight.managebac.cn/student/events.json";
+          var dateData = {
+            start: startDate.Format("yyyy-MM-dd"),
+            end: endDate.Format("yyyy-MM-dd")
+            // start: "2018-01-01",
+            // end: "2020-01-01"
+          };
+
+          $.get(
+            url,
+            dateData,
+            function (result, status) {
+              result.forEach(event => {
+                if (typeof event.id != "number") {
+                  return;
+                }
+                var event_data = {
+                  title: event.title,
+                  start: event.start,
+                  url: event.url,
+                  complete: "1",
+                  category: "",
+                  score: {
+                    get: 0,
+                    total: 0
+                  }
+                };
+                localforage.setItem(String(event.id), event_data).then(function (value) {});
+              });
+              console.log('async okk!')
+              return 1;
+            },
+            "json"
+          );
+        })
+      }
