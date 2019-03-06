39d3ca7f1e3c330946bf2605ef81ce54ca9bd5e8
Feliconut <33282070+Feliconut@users.noreply.github.com>
2019-03-06 23:39:44 +0800

    Event Handler Main Function

--- core/background.js
+++ core/background.js
@@ -8,31 +8,31 @@ var tab_Id
 
 //localforage
 //第一次安装，根据是否同意协议，引导到不同页面
-chrome.runtime.onInstalled.addListener(function () {
-  import(RUNTIME_PATH + "lib/localforage.min.js").then(() => {
-    localforage.getItem("config").then(function (value) {
-      if (value.agree == 0) {
-        chrome.tabs.create({
-          url: RUNTIME_PATH + "modules/index.html"
-        });
-      } else if (value.domain == "0") {
-        chrome.tabs.create({
-          url: RUNTIME_PATH + "modules/options.html"
-        });
-      }
-    }).catch(function (err) {
-      //值设置不正确则更新
-      jsonObj = {
-        agree: 0,
-        domain: "0",
-        subdomain: "",
-        root: ""
-      }
-      localforage.setItem("config", jsonObj);
+chrome.runtime.onInstalled.addListener(async function () {
+  await import(RUNTIME_PATH + "lib/localforage.min.js")
+  localforage.getItem("config").then(function (value) {
+    if (value.agree == 0) {
       chrome.tabs.create({
         url: RUNTIME_PATH + "modules/index.html"
       });
+    } else if (value.domain == "0") {
+      chrome.tabs.create({
+        url: RUNTIME_PATH + "modules/options.html"
+      });
+    }
+  }).catch(function (err) {
+    //值设置不正确则更新
+    jsonObj = {
+      agree: 0,
+      domain: "0",
+      subdomain: "",
+      root: ""
+    }
+    localforage.setItem("config", jsonObj);
+    chrome.tabs.create({
+      url: RUNTIME_PATH + "modules/index.html"
     });
+
   });
   //临时获取event 数据
   /*
@@ -54,6 +54,7 @@ chrome.tabs.onActivated.addListener(function (tabId) {
 })
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+
   var url = tab.url;
   var patt = new RegExp("managebac");
   if (patt.test(url)) {
@@ -61,7 +62,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     //判断tab的标题是否有竖线， MB domcontent 从 url 到title
     if (tab.status == "complete" && tab.title.indexOf("|") != -1) {
       //所有的代码从这里开始，到这里页面才完成加载。
-      eventHandler()
       console.log(tab_Id)
       var patt1 = new RegExp("student/?$"); //dashboard
       var patt2 = new RegExp("student/classes/[0-9]+/assignments"); //assignments
@@ -95,128 +95,213 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 });
 
 //Receives command and control the LocalStorage
-chrome.runtime.onMessage.addListener(function storageManager(request, sender, callback) {
-  import(RUNTIME_PATH + "lib/localforage.min.js").then(() => {
+chrome.runtime.onMessage.addListener(async function storageManager(request, sender, callback) {
+  await import(RUNTIME_PATH + "lib/localforage.min.js")
 
-    console.log(request)
-    switch (request.method) {
-      case "get":
-        {
-          var event_completed = [];
-          var event = request.event_id;
-          var length = event.length;
-          for (var i = 0; i < length; i++) {
-            var id = request.event_id[i];
+  console.log(request)
+  switch (request.method) {
+    case "get":
+      {
+        var event_completed = [];
+        var event = request.event_id;
+        var length = event.length;
+        for (var i = 0; i < length; i++) {
+          var id = request.event_id[i];
 
-            localforage.getItem(id).then(function (value) {
-                if (value.complete == 1) {
-                  event_completed.push(id);
-                }
-                var end = length - 1;
-                if (i == end) {
-                  chrome.tabs.sendMessage(sender.tab.id, {
-                    "event_id": event_completed,
-                    "type": "set_complete"
-                  });
-                }
-              })
-              .catch(function (err) {
-                console.log(err)
-              })
-
-          }
-        }
-
-      case "change_complete_status":
-        {
-
-          var length = request.event_id.length;
-          for (var i = 0; i < length; i++) {
-            event = request.event_id[i]
-
-            localforage.getItem(event).then(function (value) {
-              var jsonObj;
+          localforage.getItem(id).then(function (value) {
               if (value.complete == 1) {
-                jsonObj = value;
-                jsonObj.complete = 0;
-                localforage.setItem(event, jsonObj);
-              } else if (value.complete == 0) {
-                jsonObj = value;
-                jsonObj.complete = 1;
-                localforage.setItem(event, jsonObj);
+                event_completed.push(id);
               }
-            }).catch(function (err) {
+              var end = length - 1;
+              if (i == end) {
+                chrome.tabs.sendMessage(sender.tab.id, {
+                  "event_id": event_completed,
+                  "type": "set_complete"
+                });
+              }
+            })
+            .catch(function (err) {
               console.log(err)
             })
-          }
-        }
 
-    }
-  })
+        }
+        break;
+      }
+
+    case "change_complete_status":
+      {
+
+        var length = request.event_id.length;
+        for (var i = 0; i < length; i++) {
+          event = request.event_id[i]
+
+          localforage.getItem(event).then(function (value) {
+            var jsonObj;
+            if (value.complete == 1) {
+              jsonObj = value;
+              jsonObj.complete = 0;
+              localforage.setItem(event, jsonObj);
+            } else if (value.complete == 0) {
+              jsonObj = value;
+              jsonObj.complete = 1;
+              localforage.setItem(event, jsonObj);
+            }
+          }).catch(function (err) {
+            console.log(err)
+          })
+        }
+        break;
+      }
+    case 'refresh_fetchAll':
+      {
+        eventHandler.run(eventHandler.mode.fetchAll)
+        break;
+      }
+    case 'refresh_rollingUpdate':
+      {
+        eventHandler.run(eventHandler.mode.rollingUpdate)
+        break;
+      }
+
+  }
+
 })
 
-function eventHandler(modeBool = 0) {
+// Event Handler Object
+/*/*
+===INTERFACE===
+eventHandler.run(#<eventHandler.mode>mode, #<func>allCallback, #<func>singleCallback)
+
+#mode:
+  -> eventHandler.mode.fetchAll
+  -> eventHandler.mode.foolingUpdate
+
+  #allCallback:
+    function()
+
+  #singleCallback:
+    function(localForageKey, value)
+  
+  */
+
+
+const eventHandler = {};
+
+eventHandler.mode.fetchAll = Math.random()
+eventHandler.mode.rollingUpdate = Math.random()
+
+eventHandler.generateDates = async function (mode = null) {
+  a = await import('../lib/usefulUtil.js')
+  a.dateEnhance.init()
+  a.sayHello('dateEnhanced')
+
+  //Code begin here
+  var startDate = new Date();
+  var endDate = new Date();
+
+  switch (mode) {
+    case this.mode.fetchAll:
+      {
+        startDate.Add(-1, "y");
+        endDate.Add(1, "y");
+        break;
+      }
+    case this.mode.rollingUpdate:
+      {
+        startDate.Add(-1, "M");
+        endDate.Add(1, "M");
+        break;
+      }
+    default:
+      {
+        startDate.Add(-1, "M");
+        endDate.Add(1, "M");
+        console.log('handlerModeException, running default')
+        break;
+
+      }
+  }
+  endDate.Add(-1, "d");
+
+  var dateData = {
+    start: startDate.Format("yyyy-MM-dd"),
+    end: endDate.Format("yyyy-MM-dd")
+    // start: "2018-01-01",
+    // end: "2020-01-01"
+  };
+
+  return dateData;
+}
+
+eventHandler.query = function (dateData, allCallback = () => {}, singleCallback = () => {}) {
   Promise.all([
     import(RUNTIME_PATH + "lib/localforage.min.js"),
     import(RUNTIME_PATH + "lib/jquery-3.3.1.js")
   ]).then(() => {
     console.log('enterEventHandler')
-    import('../lib/usefulUtil.js').then((a) => {
-      a.dateEnhance.init()
-      a.sayHello('fuck')
+    console.log(dateData)
 
 
 
-      var startDate = new Date();
-      var endDate = new Date();
-      //long query
-      if (modeBool == 0) {
-        startDate.Add(-1, "y");
-        endDate.Add(1, "y");
-        //short query
-      } else {
-        startDate.Add(-1, "M");
-        endDate.Add(1, "M");
-      }
-      endDate.Add(-1, "d");
+    var url = "https://qibaodwight.managebac.cn/student/events.json";
 
+    var allCalbackParam = []
+    $.get(
+      url,
+      dateData,
+      function (result, status) {
+        result.forEach(event => {
+          if (typeof event.id != "number") {
+            return;
+          }
+          id = String(event.id);
 
-      var url = "https://qibaodwight.managebac.cn/student/events.json";
-      var dateData = {
-        start: startDate.Format("yyyy-MM-dd"),
-        end: endDate.Format("yyyy-MM-dd")
-        // start: "2018-01-01",
-        // end: "2020-01-01"
-      };
-
-      $.get(
-        url,
-        dateData,
-        function (result, status) {
-          result.forEach(event => {
-            if (typeof event.id != "number") {
-              return;
+          var event_data = {
+            title: event.title,
+            start: event.start,
+            url: event.url,
+            complete: 0,
+            category: "",
+            score: {
+              get: 0,
+              total: 0
             }
-            var event_data = {
-              title: event.title,
-              start: event.start,
-              url: event.url,
-              complete: "1",
-              category: "",
-              score: {
-                get: 0,
-                total: 0
-              }
-            };
-            localforage.setItem(String(event.id), event_data).then(function (value) {});
+          }
+          localforage.getItem(id).then(function (value) {
+            // console.log(value)
+            if (value.complete === undefined) {
+              throw "undefinedValue"
+            }
+            event_data.complete = value.complete
+            //已存在->重新写入保留complete
+
+
+          }).catch(function (err) {
+            //不存在或值设置错误->重写覆盖
+          })
+          localforage.setItem(id, event_data).then(() => {
+            allCalbackParam.push({
+              id,
+              event_data
+            })
+            singleCallback(id, event_data)
           });
-          console.log('async okk!')
+
           return 1;
-        },
-        "json"
-      );
-    })
+        })
+      },
+      "json"
+    ).then(() => {
+      allCallback(allCalbackParam)
+      console.log('async okk!')
+    });
   })
+}
+
+eventHandler.run = async function (mode = null, allCallback = () => {}, singleCallback = () => {}) {
+  dateData = await this.generateDates(mode)
+
+  await this.query(dateData, allCallback, singleCallback)
 }
 
 
