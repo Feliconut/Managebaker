585be1f9813f9e2c99f2f368a3fb5f190676f8d8
martinwang2002 <martinwang2002@gmail.com>
2019-03-07 12:54:45 +0800

    API lib + options

---
+++ WEB/API/Library/Auth/Data/auth_rule.sql
@@ -0,0 +1,10 @@
+
+    CREATE TABLE `phalapi_auth_rule` (
+                  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+                  `name` char(80) NOT NULL DEFAULT '',
+                  `title` char(20) NOT NULL DEFAULT '',
+                  `status` tinyint(1) NOT NULL DEFAULT '1',
+                  `add_condition` char(100) NOT NULL DEFAULT '',
+                  PRIMARY KEY (`id`),
+                  UNIQUE KEY `name` (`name`)
+                ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
