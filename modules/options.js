7ec1e0f2ba8b5cf995b5494509afc17bbb3ad74d
martinwang2002 <martinwang2002@gmail.com>
2019-03-12 16:19:13 +0800

    color picker and index

--- modules/options.js
+++ modules/options.js
@@ -1,5 +1,6 @@
 import('../lib/usefulUtil.js').then((a) => {
-    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('#check'));
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('#save'));
     a.dateEnhance.init();
     localforage.getItem("config").then(function (value) {
         if (value.domain != "0") {
@@ -12,6 +13,9 @@ import('../lib/usefulUtil.js').then((a) => {
             document.getElementById("urlresult").innerHTML = "OK :)";
         }
         window.mdc.autoInit();
+        $(".picker").colorPick({
+            'allowCustomColor': false
+        });
     });
 })
 
@@ -56,11 +60,8 @@ async function displayClassesOption() {
     if (!classes_list) {
         document.getElementById("urlresult").innerHTML = "failed. Please check spelling and login status.";
     }
-    window.mdc.autoInit();
-
     var i = 0;
     $("table").replaceWith('<table class="table"> <tr> <th>#</th> <th>class</th> <th>abbreviation</th> <th>color</th> <th>Method</th> </tr></table>');
-
     classes_list.forEach((thisClass) => {
         i++;
         $("table tbody:last").append('<tr> <th> <p style="margin-top:14px">' +
@@ -69,9 +70,14 @@ async function displayClassesOption() {
             thisClass.name +
             '</p> </th> <th> <div class="mdc-text-field mdc-text-field--outlined" style="width: 100%;float:left;" data-mdc-auto-init="MDCTextField"> <input type="text" id="' +
             thisClass.id +
-            '" class="mdc-text-field__input"> <div class="mdc-notched-outline"> <div class="mdc-notched-outline__leading"></div> <div class="mdc-notched-outline__notch"> </div> <div class="mdc-notched-outline__trailing"></div> </div> </div> </th> <th><span class="badge badge-pill" style="background-color:#FF304F;" title="#FF304F">&nbsp;</span></th> <th> <div class="mdc-select mdc-select--outlined" style="width:100%;float:left;" data-mdc-auto-init="MDCSelect"> <i class="mdc-select__dropdown-icon"></i> <select class="mdc-select__native-control"> <option value="" disabled selected></option> <option value="1"> percentage weights </option> <option value="2"> percentage weights with points based averaging </option> <option value="2"> absolute weights </option> </select> <div class="mdc-notched-outline"> <div class="mdc-notched-outline__leading"></div> <div class="mdc-notched-outline__notch"> </div> <div class="mdc-notched-outline__trailing"></div> </div> </div> </th> </tr>');
+            '_abbr" class="mdc-text-field__input"> <div class="mdc-notched-outline"> <div class="mdc-notched-outline__leading"></div> <div class="mdc-notched-outline__notch"> </div> <div class="mdc-notched-outline__trailing"></div> </div> </div> </th> <th><div class="picker" id="' +
+            thisClass.id +
+            '_color"></div></th> <th> <div class="mdc-select mdc-select--outlined" style="width:100%;float:left;" data-mdc-auto-init="MDCSelect"> <i class="mdc-select__dropdown-icon"></i> <select class="mdc-select__native-control"> <option value="" disabled selected></option> <option value="1"> percentage weights </option> <option value="2"> percentage weights with points based averaging </option> <option value="2"> absolute weights </option> </select> <div class="mdc-notched-outline"> <div class="mdc-notched-outline__leading"></div> <div class="mdc-notched-outline__notch"> </div> <div class="mdc-notched-outline__trailing"></div> </div> </div> </th> </tr>');
     });
-    window.mdc.autoInit();
+    $(".picker").colorPick({
+        'allowRecent': false,
+    });
+    window.mdc.autoInit( /* root */ document, () => {});
     return 1
 }
 
