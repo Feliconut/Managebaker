a147ccc4741630cb4b2a7ddb976a4564ab4abd22
martinwang2002 <martinwang2002@gmail.com>
2019-04-06 13:53:52 +0800

    help button with URL + # location

--- WEB/index.js
+++ WEB/index.js
@@ -1,1 +1,10 @@
 new WOW().init();
+
+$(function(){
+    var url = window.location.toString();
+    var id = url.split("#")[1];
+    if(id){
+        var t = $("#"+id).offset().top;
+        $(window).scrollTop(t);
+    }		   
+});
