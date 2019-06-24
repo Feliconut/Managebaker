dc5241672045921d9c456b590450b7eba9bfbdc8
martinwang2002 <martinwang2002@gmail.com>
2019-06-24 09:14:44 +0800

    fix for util tab

--- doc_handler/toolbox.js
+++ doc_handler/toolbox.js
@@ -15,7 +15,7 @@ export const addUtilitiesTab = new toolBox(
             "M 401 64 Q 170 84 150 300 Q 164 538 400 550 Q 571 535 604 382 L 664 401 Q 659 320 601 252 Q 517 277 462 337 L 532 361 Q 511 464 401 465 Q 248 447 241 299 Q 247 167 400 150 L 401 133 Q 227 150 221 299 Q 235 464 395 484 Q 520 479 549 369 L 587 379 Q 542 523 404 531 Q 187 527 173 300 Q 185 106 399 82 Z";
         var groupsTab = $("#menu > ul > li:nth-child(11)");
         var utilitiesTab = $(
-            '<li id="utilTab1" class="parent utilities"><a><svg baseProfile="full" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="100 0 560 600" preserveAspectRatio="xMinYMin meet"><path fill-rule="evenodd" fill="#859BBB" d="' +
+            '<li class="parent utilities" data-path="Utilities/\d+"><a><svg baseProfile="full" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="100 0 560 600" preserveAspectRatio="xMinYMin meet"><path fill-rule="evenodd" fill="#859BBB" d="' +
             pathString +
             '"></path></svg><span class="text-node">Utilities</span></a><ul id="utilTab2" class="nav" data-max-height="129px" style="max-height: 0px;"><li><a href="/student/profile">My Profile </a></li><li><a href="https://accounts.managebac.cn/">Launchpad </a></li><li><a href="/help">Help &amp; Support </a></li><li id="utilTab3"><a id="utilTab3" href="/logout">Logout </a></li></ul></li>'
         );
